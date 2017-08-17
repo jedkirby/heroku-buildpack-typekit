e6f13380d28fb4620180f1e970651ea2aa5415bb
James Kirby <james.kirby@jadu.co.uk>
2017-08-17 09:16:16 +0100

    Bit of tidying up, fixing a broken exception and using the new Heroku class

--- lib/typekit.py
+++ lib/typekit.py
@@ -10,17 +10,43 @@ class Typekit():
         self.token = token;
         self.endpoint = 'typekit.com';
 
+    ##
+    # Create a secure connection.
+    #
+    # @return HTTPSConnection
+    ##
     def connection(self):
         return httplib.HTTPSConnection(self.endpoint);
 
+    ##
+    # Build required request headers.
+    #
+    # @return object
+    ##
     def headers(self):
         return {
             'X-Typekit-Token': self.token
         };
 
+    ##
+    # Build params required for updating a kit.
+    #
+    # @param array domains
+    #
+    # @return string
+    ##
     def params(self, domains):
         return 'domains=' + ','.join(domains)
 
+    ##
+    # Using the connection, make a request to the API using the correct endpoint.
+    #
+    # @param string method
+    # @param string endpoint
+    # @param string body
+    #
+    # @return bool|object
+    ##
     def request(self, method, endpoint, body = ''):
         try :
             conn = self.connection();
@@ -28,27 +54,38 @@ class Typekit():
             response = conn.getresponse();
             if response.status != 200:
                 return False;
-            return json.loads(
-                response.read()
-            );
+            else
+                return json.loads(
+                    response.read()
+                );
         finally:
             conn.close();
 
+    ##
+    # Fetch a single kit using an id.
+    #
+    # @return bool|array
+    ##
     def kit(self):
-        return self.request(
-            'GET',
-            '/api/v1/json/kits/' + self.id
-        );
+        response = self.request('GET', '/api/v1/json/kits/' + self.id);
+        return response['kit'] if response else False;
 
+    ##
+    # Add domains to a single kit.
+    #
+    # @param array domains
+    #
+    # @return bool
+    ##
     def add(self, domains):
-        return self.request(
-            'POST',
-            '/api/v1/json/kits/' + self.id,
-            self.params(domains)
-        );
+        response = self.request('POST', '/api/v1/json/kits/' + self.id, self.params(domains));
+        return True if response else False;
 
+    ##
+    # Publish a single kit.
+    #
+    # @return bool
+    ##
     def publish(self):
-        return self.request(
-            'POST',
-            '/api/v1/json/kits/' + self.id + '/publish'
-        );
+        response = self.request('POST', '/api/v1/json/kits/' + self.id + '/publish');
+        return True if response else False;
